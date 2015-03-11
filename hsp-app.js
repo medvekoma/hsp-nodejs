5c029ab6384ab56e6c9b9702a68e004e03b4fc17
attila.szucs <attila.szucs@gmail.com>
2015-03-11 11:49:54 +0100

    responsive images + logo

--- hsp-app.js
+++ hsp-app.js
@@ -1,10 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 // set up handlebars view engine
-var handlebars = require('express-handlebars')
-	.create({ defaultLayout:'main' });
+var handlebars = require('express-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options) {
+            if (!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -13,28 +22,46 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/newsletter', function (req, res) {
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+app.post('/process', function (req, res) {
+    if (req.xhr || req.accepts('json,html') === 'json') {
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
+});
 app.get('/', function(req, res){
-	res.render('home');
+    res.render('home');
 });
 
 app.get('/about', function(req, res){
-	res.render('about');
+    res.render('about');
 });
 
 // custom 404 page
 app.use(function(req, res){
-	res.status(404);
-	res.render('404');
+    res.status(404);
+    res.render('404');
 });
 
 // custom 500 page
 app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.status(500);
-	res.render('500');
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
 });
 
 app.listen(app.get('port'), function(){
-	console.log( 'Express started on http://localhost:' +
-		app.get('port') + '; press Ctrl-C to terminate.' );
+    console.log( 'Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-C to terminate.' );
 });
