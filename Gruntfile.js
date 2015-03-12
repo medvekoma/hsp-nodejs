f5bd20b42a0062979bb67710c8941b1835b084b3
attila.szucs <attila.szucs@gmail.com>
2015-03-12 14:17:00 +0100

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+﻿module.exports = function(grunt) {
+    // load plugins
+    [
+        'grunt-cafe-mocha'
+    ].forEach(function(task) {
+        grunt.loadNpmTasks(task);
+    });
+    // configure plugins
+    grunt.initConfig({
+        cafemocha: {
+            all: { src: 'qa/*-tests.js', options: { ui: 'tdd' }, }
+        },
+    });
+
+    // register tasks
+    grunt.registerTask('default', ['cafemocha']);
+};
